3b770f06b3eba2d72d0a49e5f04af5b1db7fd15f
FeYuzo <felipeshida8@gmail.com>
2025-08-19 01:41:06 +0000

    Aula 18/08/25

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -2,6 +2,39 @@ package app;
 
 public class PrimeiraClasse { 
     public static void main(String[] args) {
-        System.out.println("Hello World !!!");
+        Aluno a1= new Aluno();
+        a1.nome = "Felipe";
+        a1.idade = 18;
+
+        Aluno a2 = a1;
+        a2.nome = "José";
+        
+        System.out.println("[ A1 ]");
+        System.out.println("nome = " + a1.nome);
+        System.out.println("idade = " + a1.idade);
+
+        System.out.println("[ A2 ]");
+        System.out.println("nome = " + a2.nome);
+        System.out.println("idade = " + a2.idade);
+
+        int num = 10;
+        int num2 = num;
+        num2 = 20;
+        System.out.println("[ NUM ]");
+        System.out.println("Valor = "+ num);
+
+        System.out.println("[ CHAMADA DE MÉTODO ]");
+        System.out.println("A1 = "+a1.getDados());
+        System.out.println("A2 = "+a2.getDados());
+
+        Aluno a3 = new Aluno("Maria", 16);
+        System.out.println("[ CHAMADA DE MÉTODO ]");
+        System.out.println("A3 = " + a3.getDados());
+
+        Cliente c1 = new Cliente();
+        c1.setnome("Teste");
+        System.out.println("[ CHAMADA DE GET ]");
+        System.out.println(c1.getNome());
+
     }
 }
